eab64edd28e839bf0e7792ff8ba5205ea58eb786
riverbb <157077928+riverbb@users.noreply.github.com>
2024-05-11 00:51:07 -0500

    Update point_phase.cpp

--- src/point_phase.cpp
+++ src/point_phase.cpp
@@ -1,1 +1,20 @@
-//
+//cpp
+
+#include "point_phase.h"
+
+// QUESTION 4C //////////////////////////////
+
+point_phase::point_phase(int p) : point(p) {}
+
+rollOutcome point_phase::get_outcome(roll* roll) const {
+  int roll_value = roll->roll_value();
+  if (roll_value == point) {
+    return rollOutcome::point;
+  } else if (roll_value == 7) {
+    return rollOutcome::seven_out;
+  } else {
+    return rollOutcome::nopoint;
+  }
+}
+
+/////////////////////////////////////////////
