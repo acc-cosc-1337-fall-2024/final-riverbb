77b782241e096f4a935d36a1d2465db10a7f030d
riverbb <157077928+riverbb@users.noreply.github.com>
2024-05-11 00:52:26 -0500

    Update craps_test.cpp

--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -54,4 +54,62 @@ TEST_CASE("shooter returns a value from 2 to 12") {
   }
 }
 
+// QUESTION 4 ///////////////////////////////
+
+// i don't think these work lol
+TEST_CASE("come_out_phase get_outcome") {
+  die die1;
+  die die2;
+  std::unique_ptr<come_out_phase> phase(new come_out_phase);
+
+  SECTION("natural outcome 7 or 11") {
+    std::unique_ptr<roll> roll(new roll(die1, die2));
+    int result = roll->roll_value();
+    REQUIRE((result == 7) || (result == 11));
+    REQUIRE(phase->get_outcome(roll.get()) == rollOutcome::natural);
+  }
+
+  SECTION("craps outcome 2, 3, or 12") {
+    std::unique_ptr<roll> roll(new roll(die1, die2));
+    int result = roll->roll_value();
+    REQUIRE((result == 2) || (result == 3) || (result == 12));
+    REQUIRE(phase->get_outcome(roll.get()) == rollOutcome::craps);
+  }
+
+  SECTION("point outcome 4, 5, 6, 8, 9, or 10") {
+    std::unique_ptr<roll> roll(new roll(die1, die2));
+    int result = roll->roll_value();
+    REQUIRE((result == 4) || (result == 5) || (result == 6) || (result == 8));
+    REQUIRE((result == 9) || (result == 10));
+  }
+}
+
+TEST_CASE("point_phase get_outcome") {
+  die die1;
+  die die2;
+
+  SECTION("point outcome") {
+    point_phase point_phase(5);
+    std::unique_ptr<roll> roll(new roll(die1, die2));
+    REQUIRE(roll->roll_value() != 5);
+    REQUIRE(point_phase.get_outcome(roll.get()) == rollOutcome::nopoint);
+    
+    roll.reset(new roll(die1, die2, 5));
+    REQUIRE(point_phase.get_outcome(roll.get()) == rollOutcome::point);
+  }
+
+  SECTION("seven_out outcome") {
+    point_phase point_phase(8);
+    std::unique_ptr<roll> roll(new roll(die1, die2, 7));
+    REQUIRE(point_phase.get_outcome(roll.get()) == rollOutcome::seven_out);
+  }
+
+  SECTION("nopoint outcome") {
+    point_phase point_phase(6);
+    std::unique_ptr<roll> roll(new roll(die1, die2));
+    REQUIRE(roll->roll_value() != 3);
+    REQUIRE(point_phase.get_outcome(roll.get()) == rollOutcome::nopoint);
+  }
+}
+
 /////////////////////////////////////////////
