c2e5b44c4abcfa15d20d2d4e0f6d94d84ba0cf28
riverbb <157077928+riverbb@users.noreply.github.com>
2024-05-11 00:50:35 -0500

    Update come_out_phase.cpp

--- src/come_out_phase.cpp
+++ src/come_out_phase.cpp
@@ -1,1 +1,19 @@
-//
+//cpp
+
+#include "come_out_phase.h"
+
+// QUESTION 4B //////////////////////////////
+
+rollOutcome come_out_phase::get_outcome(roll* roll) const {
+  int roll_value = roll->roll_value();
+
+  if (roll_value == 7 || roll_value == 11) {
+    return rollOutcome::natural;
+  } else if (roll_value == 2 || roll_value == 3 || roll_value == 12) {
+    return rollOutcome::craps;
+  } else {
+    return rollOutcome::point;
+  }
+}
+
+/////////////////////////////////////////////
