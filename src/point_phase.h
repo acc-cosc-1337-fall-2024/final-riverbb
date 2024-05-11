a4fb27d8466b826514be84c89917477337dae488
riverbb <157077928+riverbb@users.noreply.github.com>
2024-05-11 00:50:55 -0500

    Update point_phase.h

--- src/point_phase.h
+++ src/point_phase.h
@@ -1,1 +1,22 @@
-//
+//h
+#ifndef POINT_PHASE_H
+#define POINT_PHASE_H
+
+#include "phase.h"
+
+// QUESTION 4C //////////////////////////////
+
+// In file point_phase.h and point_phase.cpp create class PointPhase that inherits from Phase class.
+
+class point_phase : public phase {
+  public:
+    point_phase(int p);
+    virtual rollOutcome get_outcome(roll* roll) const override;
+
+  private:
+    int point;
+};
+
+/////////////////////////////////////////////
+
+#endif
