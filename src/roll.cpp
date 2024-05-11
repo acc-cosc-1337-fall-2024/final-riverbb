04b9d94f42abda21ec90b3fb123b54c35028a6aa
riverbb <157077928+riverbb@users.noreply.github.com>
2024-05-10 21:30:55 -0500

    Update roll.cpp

--- src/roll.cpp
+++ src/roll.cpp
@@ -1,1 +1,19 @@
-//
+//cpp
+
+#include "roll.h"
+
+// QUESTION 2 ///////////////////////////////
+
+roll::roll(die& die1_ref, die& die2_ref) : die1(die1_ref), die2(die2_ref), rolled_value(0) {}
+
+void roll::roll_dice() {
+  die1.roll();
+  die2.roll();
+  rolled_value = die1.roll() + die2.roll();
+}
+
+int roll::roll_value() const {
+  return rolled_value;
+}
+
+/////////////////////////////////////////////
