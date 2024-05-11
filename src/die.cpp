5aea93f35f7c198d87ce88ab737e3eba3122351f
riverbb <157077928+riverbb@users.noreply.github.com>
2024-05-10 20:59:17 -0500

    Update die.cpp

--- src/die.cpp
+++ src/die.cpp
@@ -1,1 +1,18 @@
-//
+//cpp
+
+#include "die.h"
+#include <cstdlib>
+#include <ctime>
+
+// QUESTION 1 ///////////////////////////////
+
+die::die() {
+  sides = 6;
+  srand(time(nullptr));
+}
+
+int die::roll() {
+  return rand() % sides + 1;
+}
+
+/////////////////////////////////////////////
