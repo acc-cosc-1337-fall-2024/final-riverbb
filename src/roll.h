030e05aba30f67180e49b086fe6d1a90c157d213
riverbb <157077928+riverbb@users.noreply.github.com>
2024-05-10 21:30:42 -0500

    Update roll.h

--- src/roll.h
+++ src/roll.h
@@ -1,1 +1,27 @@
-//
+//h
+
+#include "die.h"
+
+#ifndef ROLL_H
+#define ROLL_H
+
+// QUESTION 2 ///////////////////////////////
+
+//In files Roll.h and Roll.cpp, create a Roll class to simulate rolling dice (two die).
+
+class roll {
+
+  public:
+    roll(die& die1, die& die2);
+    void roll_dice();
+    int roll_value() const;
+
+  private:
+    die& die1;
+    die& die2;
+    int rolled_value;
+};
+
+/////////////////////////////////////////////
+
+#endif
