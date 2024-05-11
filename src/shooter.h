ee3adbc3b843aad78bd8cb09d997970a9dd5387c
riverbb <157077928+riverbb@users.noreply.github.com>
2024-05-10 22:33:19 -0500

    Update shooter.h

--- src/shooter.h
+++ src/shooter.h
@@ -1,1 +1,26 @@
-//
+//h
+
+#ifndef SHOOTER_H
+#define SHOOTER_H
+
+#include "roll.h"
+#include <vector>
+
+// QUESTION 3 ///////////////////////////////
+
+// In Shooter.h and Shooter.cpp , create a Shooter class to simulate shooting dice (a pair of die)
+
+class shooter {
+
+  public:
+    roll* throw_dice(die& die1, die& die2);
+    void display_rolled_values();
+    ~shooter();
+
+  private:
+    std::vector<roll*> rolls;
+};
+
+/////////////////////////////////////////////
+
+#endif
