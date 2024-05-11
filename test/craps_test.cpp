925468702b084a58a71d53d8cf63f11939921aa2
riverbb <157077928+riverbb@users.noreply.github.com>
2024-05-10 22:34:12 -0500

    Update craps_test.cpp

--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -1,6 +1,10 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 
+#include "shooter.h"
+#include "roll.h"
+#include "die.h"
+
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
@@ -28,6 +32,23 @@ TEST_CASE("roll returns a value from 2 to 12") {
   for (int i = 0; i < 10; i++) {
     roll.roll_dice();
     int result = roll.roll_value();
+    REQUIRE(result >= 2);
+    REQUIRE(result <= 12);
+  }
+}
+
+// QUESTION 3 ///////////////////////////////
+
+TEST_CASE("shooter returns a value from 2 to 12") {
+  die die1;
+  die die2;
+  shooter shooter;
+
+  for (int i = 0; i < 10; ++i) {
+    roll* roll = shooter.throw_dice(die1, die2);
+    REQUIRE(roll != nullptr);
+
+    int result = roll->roll_value();
     REQUIRE(result >= 2);
     REQUIRE(result <= 12);
   }
