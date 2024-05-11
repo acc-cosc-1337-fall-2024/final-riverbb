c85f329f1f88b4327139f469a6f385bf1df36cd9
riverbb <157077928+riverbb@users.noreply.github.com>
2024-05-11 00:50:23 -0500

    Update come_out_phase.h

--- src/come_out_phase.h
+++ src/come_out_phase.h
@@ -1,1 +1,19 @@
-//
+//h
+
+#ifndef COME_OUT_PHASE_H
+#define COME_OUT_PHASE_H
+
+#include "phase.h"
+
+// QUESTION 4B //////////////////////////////
+
+//In file come_out_phase.h and come_out_phase.cpp create class ComeOutPhase that inherits from Phase class.
+
+class come_out_phase : public phase {
+  public:
+    rollOutcome get_outcome(roll *roll) const override;
+};
+
+/////////////////////////////////////////////
+
+#endif
