85b62a1ff05bb1d62cea81eb0590f0fbfd53399d
riverbb <157077928+riverbb@users.noreply.github.com>
2024-05-11 19:18:37 -0500

    Update die.h

--- src/die.h
+++ src/die.h
@@ -3,7 +3,7 @@
 #ifndef DIE_H
 #define DIE_H
 
-// QUESTION 1 //////////////////
+// QUESTION 1 ///////////////////////////////
 
 //In files Die.h and Die.cpp, create a Die class to model a game die with 6 sides and values 1,2,3,4,5, and 6.
 
@@ -11,12 +11,12 @@ class die {
 
   public:
     die();
-    int roll();
+    int Roll();
 
   private:
     int sides;
 };
 
-////////////////////////////////
+/////////////////////////////////////////////
 
 #endif
