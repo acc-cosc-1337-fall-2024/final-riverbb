70909e57d89731df3c40ce92c8c07ffcf6f0a646
riverbb <157077928+riverbb@users.noreply.github.com>
2024-05-11 19:22:32 -0500

    Update come_out_phase.h

--- src/come_out_phase.h
+++ src/come_out_phase.h
@@ -11,7 +11,7 @@
 
 class come_out_phase : public phase {
   public:
-    rollOutcome get_outcome(roll *roll) const override;
+    RollOutcome get_outcome(Roll* Roll) const override;
 };
 
 /////////////////////////////////////////////
