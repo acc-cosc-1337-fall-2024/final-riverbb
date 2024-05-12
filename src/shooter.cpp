4c763dc5e97c0f6619fdbc035108136883d37c51
riverbb <157077928+riverbb@users.noreply.github.com>
2024-05-11 19:21:31 -0500

    Update shooter.cpp

--- src/shooter.cpp
+++ src/shooter.cpp
@@ -1,26 +1,32 @@
 //cpp
 
+#ifndef SHOOTER_CPP
+#define SHOOTER_CPP
+
 #include "shooter.h"
+#include "roll.h"
 #include <iostream>
 
 // QUESTION 3 ///////////////////////////////
 
-roll* shooter::throw_dice(die &die1, die &die2) {
-  roll* roll = new class roll(die1, die2);
-  roll->roll_dice();
-  rolls.push_back(roll);
-  return roll;
+shooter::shooter(){}
+
+Roll* shooter::throw_dice(die &die1, die &die2) {
+  Roll* Roll = new class Roll(die1, die2);
+  Roll->Roll_dice();
+  Rolls.push_back(Roll);
+  return Roll;
 }
 
-void shooter::display_rolled_values() {
-  for (roll* roll : rolls) {
-    std::cout << "Rolled value: " << roll->roll_value();
+void shooter::display_Rolled_values() {
+  for (Roll* Roll : Rolls) {
+    std::cout << "Rolled value: " << Roll->Roll_value();
   }
 }
 
 shooter::~shooter() {
-  for (roll* roll : rolls) {
-    delete roll;
+  for (Roll* Roll : Rolls) {
+    delete Roll;
   }
 }
 
